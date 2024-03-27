527c08e252b4ae559186a3dc2d47372a1cdb2474
AGCcreato <agccreato.ak@gmail.com>
2024-03-27 20:48:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
     var firebaseConfig = {
     apiKey: "AIzaSyCAUa2X-frQtrH9nJnBYranG3vb621a3X0",
     authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
+    databaseURL: "https://rat-insta-default-rtdb.firebaseio.com",
     projectId: "rat-insta",
     storageBucket: "rat-insta.appspot.com",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
+    messagingSenderId: "472010053264",
+    appId: "1:472010053264:android:d5b42b3a8a686d6aa0ea15"
   }; 
  
 ////Do Not Edit Below This
